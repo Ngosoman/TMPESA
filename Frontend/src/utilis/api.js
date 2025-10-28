38c38f3cff6da58569b64f6c1282f02db06513f5
Tom Mwambingu <[tomchom30@gmail.com]>
2025-10-28 12:50:13 +0300

    Updated Api comments

--- Frontend/src/utilis/api.js
+++ Frontend/src/utilis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/', // Your Django backend URL
+  baseURL: 'http://localhost:8000/api/', // Django backend URL
   timeout: 10000,
 });
 
