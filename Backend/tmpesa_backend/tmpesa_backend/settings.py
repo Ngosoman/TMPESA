230e20572b07c50129d40e406b5e352927c85ab9
Tom Mwambingu <[tomchom30@gmail.com]>
2025-11-01 16:19:14 +0300

    Removed cors headers

--- Backend/tmpesa_backend/tmpesa_backend/settings.py
+++ Backend/tmpesa_backend/tmpesa_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     'rest_framework_simplejwt',
     'users',
     'transactions',
--- Backend/tmpesa_backend/users/models.py
+++ Backend/tmpesa_backend/users/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class CustomUser(AbstractUser):
     mpesa_number = models.CharField(max_length=15, unique=True)
     deriv_account_id = models.CharField(max_length=50, unique=True)
-    
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
