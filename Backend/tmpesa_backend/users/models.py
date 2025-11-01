2a6ef87b38841a58114fe5018978188da159530b
Tom Mwambingu <[tomchom30@gmail.com]>
2025-11-01 16:07:16 +0300

    Implemented users models to trigger Mpesa and Validate deriv account CR

--- Backend/tmpesa_backend/users/models.py
+++ Backend/tmpesa_backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    mpesa_number = models.CharField(max_length=15, unique=True)
+    deriv_account_id = models.CharField(max_length=50, unique=True)
+    
